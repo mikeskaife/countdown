7907514c5cde55dfa9ac0adb538f5aa5aa12b9cd
Mike Skaife <mskaife@jaguarlandrover.com>
2019-01-07 15:04:55 +0000

    flesh out EventCard content and format date parts

--- EventCard.js
+++ EventCard.js
@@ -6,11 +6,32 @@ import { formatDate, getCountdownParts } from "./api";
 const styles = StyleSheet.create({});
 
 export default function EventCard({ event }) {
+  const { days, hours, minutes, seconds } = getCountdownParts(event.date);
+
   return (
     <View style={styles.card}>
       <View style={styles.cardHeader}>
         <Text style={styles.date}>{formatDate(event.date)}</Text>
         <Text style={styles.title}>{event.title}</Text>
+      </View>
+
+      <View style={styles.counterContainer}>
+        <View style={styles.counter}>
+          <Text style={styles.counterText}>{days}</Text>
+          <Text style={styles.counterLabel}>DAYS</Text>
+        </View>
+        <View style={styles.counter}>
+          <Text style={styles.counterText}>{hours}</Text>
+          <Text style={styles.counterLabel}>HOURS</Text>
+        </View>
+        <View style={styles.counter}>
+          <Text style={styles.counterText}>{minutes}</Text>
+          <Text style={styles.counterLabel}>MINUTES</Text>
+        </View>
+        <View style={styles.counter}>
+          <Text style={styles.counterText}>{seconds}</Text>
+          <Text style={styles.counterLabel}>SECONDS</Text>
+        </View>
       </View>
     </View>
   );
