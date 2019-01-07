ff1706420bfa53773458dc213c6af9a01458b9ef
Mike Skaife <mskaife@jaguarlandrover.com>
2019-01-07 15:25:25 +0000

    add timer event to cards to update timestamp every second

--- EventList.js
+++ EventList.js
@@ -1,6 +1,14 @@
 import React, { Component } from "react";
-import { Text, FlatList } from "react-native";
+import { Text, FlatList, StyleSheet } from "react-native";
 import EventCard from "./EventCard";
+
+const styles = StyleSheet.create({
+  list: {
+    flex: 1,
+    paddingTop: 20,
+    backgroundColor: "#f3f3f3"
+  }
+});
 
 class EventList extends Component {
   state = {
@@ -8,6 +16,15 @@ class EventList extends Component {
   };
 
   componentDidMount() {
+    setInterval(() => {
+      this.setState({
+        events: this.state.events.map(evt => ({
+          ...evt,
+          timer: Date.now()
+        }))
+      });
+    }, 1000);
+
     const events = require("./db.json").events.map(e => ({
       ...e,
       date: new Date(e.date)
@@ -21,6 +38,7 @@ class EventList extends Component {
         data={this.state.events}
         renderItem={({ item }) => <EventCard event={item} />}
         keyExtractor={item => item.id}
+        style={styles.list}
       />
     );
   }
